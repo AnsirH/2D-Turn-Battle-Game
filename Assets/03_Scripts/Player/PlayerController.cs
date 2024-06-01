b414f13d85bf6c634d9e87f62ac839d64e49b986
ansirH <ansir0211@gmail.com>
2024-06-01 19:00:26 +0900

    add BattleSystem, Singleton, CameraManager script and create battle field, battle camera

--- Assets/03_Scripts/Player/PlayerController.cs
+++ Assets/03_Scripts/Player/PlayerController.cs
@@ -3,21 +3,10 @@ using State;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 
 [RequireComponent(typeof(PlayerMovement),typeof(PlayerInputGetter))]
 public class PlayerController : MonoBehaviour
 {
-    //private PlayerMovement movement;
-    //private PlayerStateMachine stateMachine;
-    //private PlayerInputGetter inputGetter;
-    //private PlayerSpriteManager spriteManager;
-    //// 프로퍼티
-    //public PlayerMovement Movement => movement;
-    //public PlayerStateMachine StateMachine => stateMachine;
-    //public PlayerInputGetter InputGetter => inputGetter;
-    //public PlayerSpriteManager SpriteManager => spriteManager;
-
     public PlayerMovement Movement { get; private set; }
     public PlayerInputGetter InputGetter { get; private set; }
     public PlayerSpriteManager SpriteManager { get; private set; }
---
+++ Assets/03_Scripts/Singleton/Singleton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        instance = obj.AddComponent<T>();
+                        obj.name = typeof(T).Name;
+                    }
+                }
+                return instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
